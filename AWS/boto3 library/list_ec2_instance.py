b61dc61b727ddd763d623961a88f418ecb06cb5d
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-11-11 18:25:51 +0530

    new file:   AWS/boto3 library/list_ec2_instance.py

---
+++ AWS/boto3 library/list_ec2_instance.py
@@ -0,0 +1,9 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+response = ec2.describe_instances()
+
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print(instance['InstanceId'], instance['State']['Name'])
