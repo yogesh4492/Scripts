5730ae91e39e52fa53a7e19230811d2d40624c5c
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-11-12 17:15:54 +0530

    new file:   AWS/download/1.py 	modified:   AWS/download/down_sync_file.py 	new file:   AWS/download/list_local_folder_files.py

--- AWS/download/down_sync_file.py
+++ AWS/download/down_sync_file.py
@@ -9,19 +9,61 @@ session=boto3.Session(profile_name="Yogesh")
 app=typer.Typer()
 
 class Main:
-    def __init__(self):
-        pass
 
+
+    def __init__(self,s3path,output_folder,max_worker):
+        self.s3path=s3path
+        self.output_folder=output_folder.rstrip("/\\")
+        self.split=s3path.split("/")
+        self.bucket=self.split[2]
+        self.prefix="/".join(self.split[3:])
+        self.max_worker=max_worker
+        self.s3=session.client('s3')
+
+
+    def list_Files(self):
+        paginator=self.s3.get_paginator("list_objects_v2")
+        files=[]
+        for i in paginator.paginate(Bucket=self.bucket,Prefix=self.prefix):
+            # print("hello")
+            for obj in i.get('Contents',[]):
+                files.append(obj.get('Key'))
+        print("Total Files In Give S3 path is = ",len(files))
+        return files
+    
+
+    def download_file(self,file):
+        local=os.path.join(self.output_folder,file)
+        os.makedirs(os.path.dirname(local),exist_ok=True)
+        if os.path.exists(local):
+            return f"Skipped (exists):{local}"
+        
+        try:
+            self.s3.download_file(self.bucket,file,local)
+            return f"Downloaded: {file}"
+        except ClientError as e:
+            return f"Error : {e} "
+        
+
+    def run(self):
+        total_files=self.list_Files()
+        with Progress() as p:
+            task=p.add_task("Downloading....",total=len(total_files))
+            with ThreadPoolExecutor(max_workers=self.max_worker) as ex:
+                futures={ex.submit(self.download_file,f):f for f in total_files}
+                for i in as_completed(futures):
+                    result=i.result()
+                    p.update(task,advance=1)
+                    typer.echo(result)        
 
 @app.command()
-def main():
-    pass
+def main(s3path:str=typer.Argument(...,help="input s3path"),output_folder:str=typer.Argument(...,help="Output folder name"),max_worker:int=typer.Option(8,"--max-workers",help="Multithread Max workers")):
+    obj=Main(s3path,output_folder,max_worker)
+    obj.run()
 
 
 
-
-
-if __name__=="__main":
+if __name__=="__main__":
     app()
 
 
---
+++ AWS/download/list_local_folder_files.py
@@ -0,0 +1,23 @@
+
+import os
+import typer
+app=typer.Typer()
+def list_all_files_recursive(directory_path):
+    """Lists the full paths of all files in the directory and all subdirectories."""
+    all_files = []
+    for dirpath, dirnames, filenames in os.walk(directory_path):
+        for filename in filenames:
+            # Construct the full file path
+            full_path = os.path.join(dirpath, filename)
+            all_files.append(full_path)
+    return all_files
+
+# Example usage for the current directory and all its subfolders:
+@app.command()
+def main(input_dir:str=typer.Argument(...,help="Input Directory path or name ")):
+    all_files_list = list_all_files_recursive(input_dir)
+    print(len(all_files_list))
+    
+if __name__=="__main__":
+    app()
+
