2c1e8c07dab87e2a1b25e7674f43e51c4e3bd6c4
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-11-20 13:11:42 +0530

    modified:   GDRIVE/download/t.py

--- GDRIVE/download/t.py
+++ GDRIVE/download/t.py
@@ -1,3 +1,8 @@
-# from rich.te
+# from its get path of file but different function give different performance
+
 import os
 print(os.path.join(os.path.dirname(__file__),"..","creds.json"))
+print(os.path.dirname(__file__))
+print(os.path.relpath(__file__))
+print(os.path.realpath(__file__))
+print(os.path.abspath(__file__))
